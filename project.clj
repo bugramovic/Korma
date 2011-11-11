d8bc33c0fa239a189eb23aadf1d0b36e76a2f5c1
bugramovic <bugramovic@googlemail.com>
2011-11-11 22:01:48 +0100

    added h2 and postgres-drivers

--- project.clj
+++ project.clj
@@ -8,4 +8,7 @@
                                                         com.sun.jmx/jmxri]]
                            [org.clojure/java.jdbc "0.1.0"]]
             :autodoc {:name "Korma" :page-title "Korma Docs"}
-            :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]])
+            :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]
+
+                               [com.h2database/h2 "1.3.161"]
+                               [postgresql/postgresql "9.0-801.jdbc4"]])
